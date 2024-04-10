28df08a82d79bfcfc3553c9fce8fe052234f9628
Vinh Hoang <vinhhn3@fe.edu.vn>
2024-04-10 16:06:17 +0700

    Add new book form and functionality

--- services/add-book.php
+++ services/add-book.php
@@ -1,4 +1,34 @@
-<!--
-  Handle adding new Book
-  Then return to books.php
--->
+
+
+<!-- Insert to table -->
+
+<!-- Redirect to books.php if insert is successful -->
+
+<?php
+
+$title = $_POST['title'];
+$author = $_POST['author'];
+$price = $_POST['price'];
+
+echo $title . " " . $author . $price; // print to screen
+
+// Connect to the database
+$db = mysqli_connect('localhost', 'root', '', 'lithubbook');
+
+// Check connection
+if (!$db) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Prepare sql statement
+$sql = "INSERT INTO books (title, author, price) VALUES ('$title', '$author', '$price')";
+
+// Insert book to database
+
+if ($db->query($sql)=== TRUE){
+  echo "New record created successfully";
+  header('Location: ../pages/books.php');
+}
+else {
+  echo "Created failed";
+}
