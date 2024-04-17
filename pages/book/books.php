cb9db12a2bbdad427b56c2f3e9141f5b426e15db
Vinh Hoang <vinhhn3@fe.edu.vn>
2024-04-17 11:58:27 +0700

    Refactor add-book.html and books.php to use consistent naming for the add book functionality

--- pages/book/books.php
+++ pages/book/books.php
@@ -30,7 +30,7 @@ $result = mysqli_query($db, "SELECT * FROM books");
   <body>
     <h1>Books</h1>
     <div class="container">
-      <a href="add-book.html" class="btn btn-primary">Add new Book</a>
+      <a href="add.html" class="btn btn-primary">Add new Book</a>
       <table class="table">
         <thead>
           <tr>
