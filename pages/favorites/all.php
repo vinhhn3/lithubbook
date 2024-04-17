f68f8f7823c759a9b2e724d020e21b8b65bd1f3f
Vinh Hoang <vinhhn3@fe.edu.vn>
2024-04-17 14:28:37 +0700

    Refactor carts and favorites pages to use consistent database connection and session handling

--- pages/favorites/all.php
+++ pages/favorites/all.php
@@ -1,2 +1,53 @@
-<!-- Display all the favorites of the user -->
-<!-- Have the button to remove favorite -->
+<?php
+session_start(); // start a session
+
+if (!isset($_SESSION['user_id'])) {
+  echo ("Not authrozied");
+  exit;
+}
+
+// use mysqli to connection
+$host = 'localhost';
+$db = 'lithubbook';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+// Connect to the database
+
+$db = mysqli_connect($host, $user, $pass, $db);
+
+// Check connection
+if (!$db) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$result = mysqli_query($db, "SELECT * FROM favorites WHERE user_id = " . $_SESSION['user_id']);
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <table>
+    <tr>
+      <th>User ID</th>
+      <th>Book ID</th>
+    </tr>
+    <?php while ($row = mysqli_fetch_assoc($result)): ?>
+      <tr>
+        <td><?php echo $row['user_id']; ?></td>
+        <td><?php echo $row['book_id']; ?></td>
+      </tr>
+    <?php endwhile; ?>
+  </table>
+</body>
+
+</html>
